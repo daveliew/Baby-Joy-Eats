d4e583cd4f64f6eb6544ab7b939db90cb073e038
daveliew <daveliew@gmail.com>
2021-07-06 02:11:49 +0800

    EOD commit

--- src/components/Ajax/IngredientAjax.js
+++ src/components/Ajax/IngredientAjax.js
@@ -1,12 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext, useState } from "react";
+// import { DataContext } from "./App";
+import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
+
+const Autocomplete = styled.div`
+  display: relative;
+`;
 
 const IngredientAjax = () => {
+  //   const value = useContext(DataContext);
+  //   const { dispatch, ACTIONS } = value;
+  const [ingredient, setIngredient] = useState({ id: "", content: "" });
   const [data, setData] = useState(null);
   const [toggle, setToggle] = useState(false);
+  const [query, setQuery] = useState("");
 
   const handleToggle = () => {
     console.log("click");
     setToggle(!toggle);
+  };
+
+  const handleChange = (event) => {
+    // const name = event.target.name;
+    // console.log("handleChange - event", event.target.computedName);
+
+    const value = event.target.value;
+    console.log("handleChange - value", event.target.value);
+
+    setQuery(value);
+    setIngredient({
+      ...ingredient,
+      content: value,
+    });
+    //!add a dispatch
   };
 
   //! rewrite this such that we search on form submit
@@ -14,8 +40,8 @@ const IngredientAjax = () => {
     const autoComplete =
       "https://api.spoonacular.com/food/ingredients/autocomplete";
     const API_ROOT = autoComplete;
-    const queryItem = "apple";
-    const URL = `${API_ROOT}${queryItem}&apiKey=${process.env.REACT_APP_SPOONACULAR}`;
+    const queryItem = query;
+    const URL = `${API_ROOT}?query=${queryItem}&apiKey=${process.env.REACT_APP_SPOONACULAR}`;
     console.log(URL);
 
     fetch(URL)
@@ -32,22 +58,42 @@ const IngredientAjax = () => {
       .catch((error) => {
         console.log("error");
       });
-  }, []);
+  }, [query]);
 
   //   return <div>Hello from Ajax</div>;
-
-  return data === null ? (
+  return (
     <>
-      <h1>Hello from Fetch</h1>
-      <button onClick={handleToggle}> Add Ingredient </button>
-    </>
-  ) : (
-    <>
-      <button onClick={handleToggle}> Add Ingredient </button>
-      {/* <h1>{data.title}</h1>
-      <p>Likes: {data.likes}</p> */}
+      <Autocomplete>
+        <form>
+          <input
+            onChange={handleChange}
+            value={ingredient.content}
+            options={data}
+            type="text"
+            className="form-control"
+            name="name"
+            placeholder="Add an ingredient"
+            autoComplete="off"
+          />
+          <button onClick={handleToggle}> Fetch Ingredient </button>
+        </form>
+      </Autocomplete>
     </>
   );
+
+  //   return data === null ? (
+  //     <>
+  //       <h1>Hello from Fetch</h1>
+  //       <button onClick={handleToggle}> Add Ingredient </button>
+  //     </>
+  //   ) : (
+  //     <>
+  //     <h1></h1>
+  //       <button onClick={handleToggle}> Add Ingredient </button>
+  //       {/* <h1>{data.title}</h1>
+  //       <p>Likes: {data.likes}</p> */}
+  //     </>
+  //   );
 };
 
 export default IngredientAjax;
