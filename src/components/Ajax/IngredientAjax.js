de8d57756c0d89e1a5cad203d3bf9c448fa0045f
daveliew <daveliew@gmail.com>
2021-07-06 13:05:26 +0800

    autocomplete trial phase 1 passed

--- src/components/Ajax/IngredientAjax.js
+++ src/components/Ajax/IngredientAjax.js
@@ -1,19 +1,27 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Autosuggest from "react-autosuggest";
 import axios from "axios";
+import { DataContext } from "../App";
+import { v4 as uuidv4 } from "uuid";
+import ComboBox from "./Test";
 
 const IngredientAjax = () => {
-  //   const value = useContext(DataContext);
-  //   const { dispatch, ACTIONS } = value;
   const [ingredient, setIngredient] = useState({ id: "", content: "" });
   const [data, setData] = useState([{ test: "test" }]);
   const [toggle, setToggle] = useState(false);
-  const [query, setQuery] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
+  const [query, setQuery] = useState([{ test: "test" }]);
+  //   const [suggestions, setSuggestions] = useState([]);
+
+  const value = useContext(DataContext);
+  const { dispatch, ACTIONS } = value;
 
   const handleToggle = () => {
-    console.log("click");
+    console.log("query", query);
     setToggle(!toggle);
+    dispatch({
+      type: ACTIONS.ADD_INGREDIENT,
+      payload: { id: uuidv4(), content: query },
+    });
   };
 
   const handleChange = (event) => {
@@ -28,7 +36,6 @@ const IngredientAjax = () => {
       ...ingredient,
       content: value,
     });
-    //!add a dispatch
   };
 
   //! rewrite this such that we search on form submit
@@ -40,80 +47,40 @@ const IngredientAjax = () => {
     const URL = `${API_ROOT}?query=${queryItem}&apiKey=${process.env.REACT_APP_SPOONACULAR}`;
     console.log(URL);
 
-    //     fetch(URL)
-    //       .then((res) => {
-    //         if (res.ok) {
-    //           return res.json();
-    //         }
-    //         throw new Error("Bad Response from Server");
-    //       })
-    //       .then((data) => {
-    //         console.log(data);
-    //         setData(data);
-    //       })
-    //       .catch((error) => {
-    //         console.log("error");
-    //       });
+    fetch(URL)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Bad Response from Server");
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
   }, [query]);
 
   //   return <div>Hello from Ajax</div>;
   return (
     <>
-      {/* <input
-          onChange={handleChange}
-          value={ingredient.content}
-          options={data}
-          type="text"
-          className="form-control"
-          name="name"
-          placeholder="Add an ingredient"
-          autoComplete="off"
-        /> */}
-      <button onClick={handleToggle}> Fetch Ingredient </button>
-      {/* <Autosuggest
-        inputProps={{
-          placeholder: "Add ingredient",
-          autoComplete: "abcd",
-          name: "food",
-          id: "food",
-          onChange: { handleChange },
-        }}
-        suggestions={suggestions}
-        onSuggestionsFetchRequested={async ({ value }) => {
-          if (!value) {
-            setSuggestions([]);
-            return;
-          }
-          try {
-            const result = await axios.get(
-              `https://api.spoonacular.com/food/ingredients/autocomplete?query=${value}&apiKey=${process.env.REACT_APP_SPOONACULAR}`
-            );
-            setSuggestions(result);
-            console.log(result.data);
-          } catch (e) {
-            setSuggestions([]);
-            console.log(e);
-          }
-        }}
-        getSuggestionValue={(suggestion) => suggestions.name}
-        renderSuggestion={(suggestion) => <div>{suggestions.name}</div>}
-      /> */}
+      <input
+        onChange={handleChange}
+        value={ingredient.content}
+        options={data}
+        type="text"
+        className="form-control"
+        name="name"
+        placeholder="Add an ingredient"
+        autoComplete="off"
+      />
+
+      <button onClick={handleToggle}> Add to Ingredient List </button>
+      <ComboBox value={data} />
     </>
   );
-
-  //   return data === null ? (
-  //     <>
-  //       <h1>Hello from Fetch</h1>
-  //       <button onClick={handleToggle}> Add Ingredient </button>
-  //     </>
-  //   ) : (
-  //     <>
-  //     <h1></h1>
-  //       <button onClick={handleToggle}> Add Ingredient </button>
-  //       {/* <h1>{data.title}</h1>
-  //       <p>Likes: {data.likes}</p> */}
-  //     </>
-  //   );
 };
 
 export default IngredientAjax;
