7844474f11363bc8ca04a0c58e443f0265a7086f
daveliew <daveliew@gmail.com>
2021-07-08 18:30:14 +0800

    style: 💄 info card skeleton

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Collapse,
+  IconButton,
+  Typography,
+} from "@material-ui/core/";
+import { red } from "@material-ui/core/colors";
+
 import dataReturn from "../data/dataReturnById";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: "50vw",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
 // import Ajax from "../components/Ajax/Ajax";
-
 // const instructions = dataReturn.analyzedInstructions[0].steps[0].step;
-
 // console.log(dataReturn.analyzedInstructions[0].steps);
 
 const instructionsArr = dataReturn.analyzedInstructions[0].steps;
+
 let instructionSteps = instructionsArr.map((step, index) => {
   return (
     <div key={index}>
@@ -18,37 +55,46 @@ let instructionSteps = instructionsArr.map((step, index) => {
   );
 });
 
-// const generateRow = (arr) => {
-//   let images = arr.map((image, index) => {
-//     return <img src={arr[index].image} alt="" key="index"></img>;
-//   });
-//   return images;
-// };
-
-// let instructionIngredients = instructionsArr.map((step, index) => {
-//   return (
-//     <div key={index}>
-//       <span>
-//         {index + 1}) {instructionsArr[index].step}
-//       </span>
-//       {generateRow(instructionsArr[index].ingredients)}
-//     </div>
-//   );
-// });
-
 const Info = () => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <>
-      <div className="info-card">
+    <Card className={classes.root}>
+      <CardContent>
         <h1>{dataReturn.title}</h1>
         <img src={dataReturn.image} alt={dataReturn.title}></img>
         <h3>Cooking Instructions</h3>
-        <div>{instructionSteps}</div>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {instructionSteps}
+        </Typography>
+
         <hr />
         <button>Get another recipe?</button>
-      </div>
-    </>
+      </CardContent>
+    </Card>
   );
 };
-
 export default Info;
+
+// // const generateRow = (arr) => {
+// //   let images = arr.map((image, index) => {
+// //     return <img src={arr[index].image} alt="" key="index"></img>;
+// //   });
+// //   return images;
+// // };
+
+// // let instructionIngredients = instructionsArr.map((step, index) => {
+// //   return (
+// //     <div key={index}>
+// //       <span>
+// //         {index + 1}) {instructionsArr[index].step}
+// //       </span>
+// //       {generateRow(instructionsArr[index].ingredients)}
+// //     </div>
+// //   );
+// // });
